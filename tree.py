e07cca0e01079d85f951596cdb0ccb6488d7113e
stealthycoin <john.w.carlyle@gmail.com>
2013-04-24 19:04:42 -0700

    Added colorization

--- tree.py
+++ tree.py
@@ -5,38 +5,43 @@ import math
 import random
 
 def endPoint(start, length, angle):
+    """Calculate endpoint of a line taht starts at loc, with a given length and angle"""
     return (round(start[0] + length*math.cos(angle)), 
             round(start[1] - length*math.sin(angle)))
 
 def genAngle():
-    """Generates an angle from pi/2 to pi/6"""
+    """Generates an angle from pi10 to 2*pi/3"""
     return (2*math.pi/3 - math.pi/10) * random.random() + math.pi/10
     
+def dColor(color):
+    return (max(min(color[0]+3,255),0),
+            max(min(color[1],255),0),
+            max(min(color[2]+15,255),0))
+    
 
-def drawTree(s, loc, length, angle, n):
+def drawTree(s, color, loc, length, angle, n):
+    """Draws a segment of the tree, and then draws more"""
     if n > 0:
         ep = endPoint(loc, length, angle)
-        pygame.draw.aaline(s, (255,150,255), loc, ep)
+        pygame.draw.line(s, color, loc, ep)
         a = genAngle()
-        drawTree(s, ep, 2*length/3, angle+a/2, n-1)
-        drawTree(s, ep, 2*length/3, angle-a/2, n-1)
+        drawTree(s, dColor(color), ep, 2*length/3, angle+a/2, n-1)
+        drawTree(s, dColor(color), ep, 2*length/3, angle-a/2, n-1)
 
 def main():
-    random.seed()
     w = 512
     h = 512
     z = w,h
     d = pygame.display
     s = d.set_mode(z)
-    pygame.draw.rect(s, (0,0,0), (0,0,w,h))
+    pygame.draw.rect(s, (60,60,60), (0,0,w,h))
     
     #args
     if len(sys.argv) > 1:
         depth = int(sys.argv[1])
     else: 
         depth = 1
-    pygame.draw.rect(s, (0,0,0), (0,0,w,w))
-    drawTree(s, (round(w/2),h), h/4, math.pi/2, depth)
+    drawTree(s, (0,0,50), (round(w/2),h), h/4, math.pi/2, depth)
     d.flip()
         
     b = True
