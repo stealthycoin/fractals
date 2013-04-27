01db958004a3a2c9327d3210447f4a98324cefad
stealthycoin <john.w.carlyle@gmail.com>
2013-04-27 02:55:37 -0700

    Nothing useful done here

--- kochSnowflake.py
+++ kochSnowflake.py
@@ -1,8 +1,20 @@
+import math
 import sys
 import pygame
 
-def ln(length, angle, depth, s):
-    
+def endPoint(start, length, angle):
+    """Calculate endpoint of a line taht starts at loc, with a given length and angle"""
+    return (round(start[0] + length*math.cos(angle)), 
+            round(start[1] - length*math.sin(angle)))
+
+
+def ln(sp, length, angle, depth, s):
+    if depth > 0:
+        pygame.draw.aaline(s, (255,255,255), sp, endPoint(sp, length / 3, angle))
+        ln(sp, length / 3, angle + math.pi / 3, depth - 1, s)
+        pygame.draw.aaline(s, (255,255,255), endPoint(sp, 2.0*length/3.0, angle), endPoint(sp, length, angle))
+
+
 
 w = 512
 z = w,w
@@ -15,7 +27,7 @@ if len(sys.argv) > 1:
 else: 
     depth = 1
 
-ln(
+ln((5,w+10),w-10,0,2,s)
 d.flip()
 b = True
 while b:
